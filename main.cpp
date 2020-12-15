5f4610d94b162e5e57a3f2697806d6f998ca9a08
ztygreat <ztygreat@foxmail.com>
2020-12-15 21:12:09 +0800

    解压优化

--- main.cpp
+++ main.cpp
@@ -3,18 +3,15 @@
 #include<fstream>
 #include <cstring>
 
-int main(int argc, char* argv[]) {
-    /*if(argc == 2){
-        if((argv[1][0] == '-') && (argv[1][1] == 'd'))
 
-            decompress();
-    }
-    else
-        compress();*/
-    int func;//func为1就压缩，为2就解压
+
+int main(int argc, char* argv[]) {
+    
+    int func;
     string infilename, outfilename;
     cin >> func >> infilename >> outfilename;
     Huffman Test(func, infilename, outfilename);
 
     return 0;
 }
+
